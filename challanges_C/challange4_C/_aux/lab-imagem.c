efeb646a78b1effb6e7ad3a899b17134bc1c3342
Vinicius <vinicius_andrade2010@hotmail.com>
2023-10-09 18:18:20 -0300

    Build [challange04] : Iniciando desafio 4 de LP parte2

--- challanges_C/challange4_C/_aux/lab-imagem.c
+++ challanges_C/challange4_C/_aux/lab-imagem.c
@@ -5,75 +5,74 @@
 
 #define MAX 129
 
-void readImage(char *name, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int *cols, int *rows){
-	int i, j, lixo;
-	char p;
+void readImage(char *name, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int *cols, int *rows) {
+    int i, j, lixo;
+    char p;
 
-	FILE *f;
-	f = fopen(name, "r");
-	if(f==NULL){
-		printf("Erro ao abrir a imagem \"%s\".\n", name);
-		return;
-	}
-	fscanf(f, "%c", &p);
-	fscanf(f, "%d", &lixo);
-	fscanf(f, "%d %d", cols, rows);
-	fscanf(f, "%d", &lixo);
-	for(i=0;i<(*rows);i++){
-		for(j=0;j<(*cols);j++){
-			fscanf(f, "%d %d %d", &R[i][j], &G[i][j], &B[i][j]);
-		}
-	}
-	fclose(f);
+    FILE *f;
+    f = fopen(name, "r");
+    if (f == NULL) {
+        printf("Erro ao abrir a imagem \"%s\".\n", name);
+        return;
+    }
+    fscanf(f, "%c", &p);
+    fscanf(f, "%d", &lixo);
+    fscanf(f, "%d %d", cols, rows);
+    fscanf(f, "%d", &lixo);
+    for (i = 0; i < (*rows); i++) {
+        for (j = 0; j < (*cols); j++) {
+            fscanf(f, "%d %d %d", &R[i][j], &G[i][j], &B[i][j]);
+        }
+    }
+    fclose(f);
 }
 
-void writeImage(char *arqSaida, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int cols, int rows){
-  	int i, j;
-  	FILE *f;
+void writeImage(char *arqSaida, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int cols, int rows) {
+    int i, j;
+    FILE *f;
 
-  	f = fopen(arqSaida, "w");
-  	fprintf(f, "P3\n%d %d\n255\n",cols, rows);
+    f = fopen(arqSaida, "w");
+    fprintf(f, "P3\n%d %d\n255\n", cols, rows);
 
-	for(i=0;i<rows;i++){
-		for(j=0;j<cols;j++){
-			fprintf(f, "%d %d %d", R[i][j], G[i][j], B[i][j]);
-			if (j<cols-1){
-				fprintf(f, " ");
-			}
-		}
-		fprintf(f, "\n");
-	}
-	fclose(f);
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            fprintf(f, "%d %d %d", R[i][j], G[i][j], B[i][j]);
+            if (j < cols - 1) {
+                fprintf(f, " ");
+            }
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
 }
 
-void esticarContraste(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX], int novaB[MAX][MAX], int cols, int rows){
-	// Insira seu código aqui
+void esticarContraste(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX],
+                      int novaB[MAX][MAX], int cols, int rows) {
 }
 
-void escalaCinza(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX], int novaB[MAX][MAX], int cols, int rows){
-	// Insira seu código aqui
+void escalaCinza(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX],
+                 int novaB[MAX][MAX], int cols, int rows) {
 }
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    fprintf(stderr, "Argumentos invalidos.\n");
-    fprintf(stderr, "Usado:");
-    for (int i=0; i<argc; i++) {
-			fprintf(stderr, " %s", argv[i]);
+    if (argc != 3) {
+        fprintf(stderr, "Argumentos invalidos.\n");
+        fprintf(stderr, "Usado:");
+        for (int i = 0; i < argc; i++) {
+            fprintf(stderr, " %s", argv[i]);
+        }
+        fprintf(stderr, "\n");
+        return 1;
     }
-    fprintf(stderr, "\n");
-    return 1;
-  }
-  char efeito[20];
-  char *arqEntrada = argv[1];
-  char *arqSaida = argv[2];
-  int R[MAX][MAX], G[MAX][MAX], B[MAX][MAX], novaR[MAX][MAX], novaG[MAX][MAX], novaB[MAX][MAX];
-  int cols, rows;
+    char efeito[20];
+    char *arqEntrada = argv[1];
+    char *arqSaida = argv[2];
+    int R[MAX][MAX], G[MAX][MAX], B[MAX][MAX], novaR[MAX][MAX], novaG[MAX][MAX], novaB[MAX][MAX];
+    int cols, rows;
 
-  scanf("%s", efeito);
-
-	// Insira seu código aqui
+    scanf("%s", efeito);
 
 
-  return 0;
+
+    return 0;
 }
